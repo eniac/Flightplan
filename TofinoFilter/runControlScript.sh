0055f99f7f5082d19b0694f7c4bc76a7fc1d3e56
John Sonchack <jsonch@seas.upenn.edu>
2017-11-15 20:30:50 +0000

    control scripts cleanup. 100G from host

--- TofinoFilter/runControlScript.sh
+++
@@ -1,4 +0,0 @@
-# set paths and start script
-export PATH=$SDE_INSTALL/bin:$PATH
-export PYTHONPATH=$SDE_INSTALL/lib/python2.7/site-packages/p4testutils:$SDE_INSTALL/lib/python2.7/site-packages/tofinopd/:$SDE_INSTALL/lib/python2.7/site-packages/tofino:$SDE_INSTALL/lib/python2.7/site-packages/:$PYTHONPATH
-python controlScript.py
