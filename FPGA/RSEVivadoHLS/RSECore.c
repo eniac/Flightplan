b83e4e8c45d9cb37bec67804252bcc10283c2c10
Hans Giesen <giesen@seas.upenn.edu>
2018-02-07 14:20:18 -0500

    Changed encoder such that it adds a new header to the encoded packets.

--- FPGA/RSEVivadoHLS/RSECore.c
+++ FPGA/RSEVivadoHLS/RSECore.c
@@ -3,9 +3,15 @@
 
 #include "Encoder.h"
 
-#define FEC_PACKET_SIZE 368
-#define BYTES_PER_PACKET ((FEC_PACKET_SIZE + 8 - 1) / 8)
-#define WORDS_PER_PACKET ((FEC_PACKET_SIZE + 64 - 1) / 64)
+#define ETH_HEADER_SIZE (112)
+#define TAG_SIZE (24)
+#define PAYLOAD_SIZE (256)
+
+#define INPUT_PACKET_SIZE (ETH_HEADER_SIZE + TAG_SIZE + PAYLOAD_SIZE)
+#define OUTPUT_PACKET_SIZE (ETH_HEADER_SIZE + TAG_SIZE + INPUT_PACKET_SIZE)
+#define BYTES_PER_INPUT_PACKET ((INPUT_PACKET_SIZE + 8 - 1) / 8)
+#define BYTES_PER_OUTPUT_PACKET ((OUTPUT_PACKET_SIZE + 8 - 1) / 8)
+#define WORDS_PER_OUTPUT_PACKET ((OUTPUT_PACKET_SIZE + 64 - 1) / 64)
 
 #define OP_START_ENCODER    (1 << 0)
 #define OP_ENCODE_PACKET    (1 << 1)
@@ -26,68 +32,11 @@ typedef struct
   uint1 Start_of_frame;
 } interface;
 
-static fec_sym parity_buffer[BYTES_PER_PACKET][FEC_MAX_H];
+static fec_sym parity_buffer[BYTES_PER_INPUT_PACKET][FEC_MAX_H];
 
 void Matrix_multiply_HW(fec_sym Data[FEC_MAX_K], fec_sym Parity[FEC_MAX_H], int k, int h);
 
-#if 0
-void RSE_core(uint8 operation, uint32 index, uint1 is_parity, packet_t data, packet_t * parity)
-{
-#pragma HLS ARRAY_PARTITION variable=parity_buffer complete dim=0
-  int k = FEC_MAX_K;
-  int h = FEC_MAX_H;
-
-  *parity = 0;
-
-  if (operation & OP_START_ENCODER)
-  {
-    for (int i = 0; i < BYTES_PER_PACKET; i++)
-    {
-#pragma HLS unroll
-      for (int j = 0; j < FEC_MAX_H; j++)
-      {
-#pragma HLS unroll
-        parity_buffer[i][j] = 0;
-      }
-    }
-  }
-
-  if (operation & OP_ENCODE_PACKET)
-  {
-    for (int i = 0; i < BYTES_PER_PACKET; i += UNROLL_FACTOR)
-    {
-#pragma HLS DEPENDENCE variable=parity_buffer inter false
-#pragma HLS pipeline
-      /*
-       * The loop over j should not be necessary, but if I remove it and use an unroll pragma instead,
-       * the synthesis tool finds false dependencies between different elements of parity_buffer.  I
-       * suspect that the compiler tries to save loads and stores by combining loads/stores to adjacent
-       * elements.  If I set an HLS dependency pragma to resolve it, the tool fails because it perceives
-       * the dependencies as real dependencies.  By separating the loops, we do not flag the new
-       * dependencies caused by unrolling as false dependencies.
-       */
-      for (int j = 0; j < UNROLL_FACTOR; j++)
-      {
-        if (i + j < BYTES_PER_PACKET)
-        Incremental_encode(data & ((1 << FEC_M) - 1), parity_buffer[i + j], index, h,
-            operation & OP_START_ENCODER);
-        data >>= FEC_M;
-      }
-    }
-  }
-  else if (operation & OP_GET_ENCODED)
-  {
-    for (int i = 0; i < BYTES_PER_PACKET; i++)
-    {
-#pragma HLS unroll
-      *parity |= parity_buffer[i][index];
-      *parity <<= FEC_M;
-    }
-  }
-}
-#endif
-
-void RSE_core(tuple Tuple, interface Data[WORDS_PER_PACKET], interface Parity[WORDS_PER_PACKET])
+void RSE_core(tuple Tuple, interface * Data, interface Parity[WORDS_PER_OUTPUT_PACKET])
 {
 #pragma HLS DATA_PACK variable=Tuple
 #pragma HLS DATA_PACK variable=Data
@@ -98,22 +47,10 @@ void RSE_core(tuple Tuple, interface Data[WORDS_PER_PACKET], interface Parity[WO
 
 #pragma HLS ARRAY_PARTITION variable=parity_buffer complete dim=0
 
-  if (Tuple.Operation & OP_START_ENCODER)
-  {
-    for (int i = 0; i < BYTES_PER_PACKET; i++)
-    {
-#pragma HLS unroll
-      for (int j = 0; j < FEC_MAX_H; j++)
-      {
-#pragma HLS unroll
-        parity_buffer[i][j] = 0;
-      }
-    }
-  }
-
   if (Tuple.Operation & OP_ENCODE_PACKET)
   {
-    for (int i = 0; i < WORDS_PER_PACKET; i++)
+    int i = 0;
+    while (1)
     {
 #pragma HLS DEPENDENCE variable=parity_buffer inter false
 #pragma HLS pipeline
@@ -128,7 +65,7 @@ void RSE_core(tuple Tuple, interface Data[WORDS_PER_PACKET], interface Parity[WO
       uint64 Word = Data[i].Data;
       for (int j = 0; j < 8; j++)
       {
-        if (8 * i + j < BYTES_PER_PACKET)
+        if (8 * i + j < BYTES_PER_INPUT_PACKET)
         {
           fec_sym Symbol = (Word >> (7 - j)) & 0xFF;
           Incremental_encode(Symbol, parity_buffer[8 * i + j], Tuple.Index, FEC_MAX_H,
@@ -141,25 +78,51 @@ void RSE_core(tuple Tuple, interface Data[WORDS_PER_PACKET], interface Parity[WO
       Parity[i].Start_of_frame = Data[i].Start_of_frame;
       Parity[i].Count = Data[i].Count;
       Parity[i].Error = Data[i].Error;
+
+      if (Data[i].End_of_frame)
+        break;
+
+      i++;
     }
   }
   else
   {
-    for (int i = 0; i < WORDS_PER_PACKET; i++)
+    int Input_finished = 0;
+    for (int i = 0; i < WORDS_PER_OUTPUT_PACKET; i++)
     {
 #pragma HLS pipeline
-      uint64 Word = 0;
+
+      interface Input;
+      uint64 Header_word = 0;
+      if (!Input_finished)
+        Input = Data[i];
+
+      Header_word = Input.Data;
+      Input_finished = Input.End_of_frame;
+
+      uint64 Output_word = 0;
+      int Position = 0;
       for (int j = 0; j < 8; j++)
       {
-        if (8 * i + j < BYTES_PER_PACKET)
-          Word |= parity_buffer[8 * i + j][Tuple.Index] & 0xFF;
-        Word <<= 8;
+        Output_word <<= 8;
+        if (64 * i + 8 * j < ETH_HEADER_SIZE + TAG_SIZE)
+        {
+          Output_word |= (Header_word >> 8 * j) & 0xFF;
+        }
+        else if (Position < BYTES_PER_INPUT_PACKET)
+        {
+          Output_word |= parity_buffer[Position][Tuple.Index] & 0xFF;
+          Position++;
+        }
       }
-      Parity[i].Data = Word;
-      Parity[i].End_of_frame = Data[i].End_of_frame;
-      Parity[i].Start_of_frame = Data[i].Start_of_frame;
-      Parity[i].Count = Data[i].Count;
-      Parity[i].Error = Data[i].Error;
+
+      int Last_word = (i == WORDS_PER_OUTPUT_PACKET - 1);
+
+      Parity[i].Data = Output_word;
+      Parity[i].Start_of_frame = (i == 0);
+      Parity[i].End_of_frame = Last_word;
+      Parity[i].Count = Last_word ? (8 * WORDS_PER_OUTPUT_PACKET - BYTES_PER_OUTPUT_PACKET) : 8;
+      Parity[i].Error = 0;
     }
   }
 }
