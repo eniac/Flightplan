f9551dda677796c6de90672355989a6370a4bb3b
Hans Giesen <giesen@seas.upenn.edu>
2018-02-23 15:55:05 -0500

    Removed outdated copy of encoder.

--- rseAccelerated/rseAcceleratedTest.h
+++
@@ -1,5 +0,0 @@
-#include "rse.h"
-
-void Matrix_multiply_HW(fec_sym Data[FEC_MAX_K], fec_sym Parity[FEC_MAX_H], int k, int h);
-
-void Verify_matrix_multiply(int Data_packet_count, int Parity_packet_count, int Position);
