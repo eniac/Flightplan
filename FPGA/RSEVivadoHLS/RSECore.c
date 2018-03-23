dad790cc0b1f31e7ff50e0a26d828d219ef9181c
Hans Giesen <giesen@seas.upenn.edu>
2018-03-23 16:53:02 -0400

    Completed version of RSE core that can handle variable-length packets.

--- FPGA/RSEVivadoHLS/RSECore.c
+++ FPGA/RSEVivadoHLS/RSECore.c
@@ -42,7 +42,7 @@ typedef struct
 static fec_sym Parity_buffer[FEC_MAX_PACKET_SIZE][FEC_MAX_H];
 
 void RSE_core(tuple_interface Tuple, packet_interface * Data,
-    packet_interface Parity[1600])
+    packet_interface Parity[FEC_MAX_PACKET_SIZE])
 {
 #pragma HLS DATA_PACK variable=Tuple
 #pragma HLS DATA_PACK variable=Data
@@ -59,58 +59,88 @@ void RSE_core(tuple_interface Tuple, packet_interface * Data,
 #pragma HLS ARRAY_PARTITION variable=Parity_buffer complete dim=2
 //#pragma HLS ARRAY_RESHAPE variable=Parity_buffer cyclic factor=4 dim=2
 
-  int Word_offset = 0;
-  int Input_finished = 0;
-  int End = 0;
-  int Packet_length = FEC_ETH_HEADER_SIZE / 8;
-  int Prev_word_size = 0;
-  do
+  if (!Tuple.Valid)
   {
+    unsigned Word_offset = 0;
+    unsigned End = 0;
+    do
+    {
+#pragma HLS LOOP_TRIPCOUNT min=8 max=190
+#pragma HLS pipeline
+
+      packet_interface Input = Data[Word_offset];
+      End = Input.End_of_frame;
+      Parity[Word_offset++] = Input;
+    }
+    while (!End);
+  }
+  else if (Tuple.Operation & FEC_OP_ENCODE_PACKET)
+  {
+    unsigned Word_offset = 0;
+    unsigned End = 0;
+    do
+    {
 #pragma HLS DEPENDENCE variable=Parity_buffer inter false
 #pragma HLS LOOP_TRIPCOUNT min=8 max=190
 #pragma HLS pipeline
 
-    Packet_length += Prev_word_size;
+      packet_interface Input = Data[Word_offset];
+      packet_interface Output = Input;
 
-    packet_interface Input;
-    if (!Input_finished)
-    {
-      Input = Data[Word_offset];
-      Prev_word_size = Input.Count;
-    }
-    else
-      Prev_word_size = 0;
-
-    Input_finished = Input.End_of_frame;
-
-    packet_interface Output;
-
-    if (!Tuple.Valid)
-    {
-      Output = Input;
       End = Input.End_of_frame;
-    }
-    else if (Tuple.Operation & FEC_OP_ENCODE_PACKET)
-    {
-      int Offset = 8 * Word_offset;
-      for (int Byte_offset = 0; Byte_offset < 8; Byte_offset++)
+
+      unsigned Offset = 8 * Word_offset;
+      for (unsigned Byte_offset = 0; Byte_offset < 8; Byte_offset++)
       {
         if (Byte_offset < Input.Count)
         {
           fec_sym Symbol = (Input.Data >> 8 * (7 - Byte_offset)) & 0xFF;
           Incremental_encode(Symbol, Parity_buffer[Offset], Tuple.Index,
-              FEC_MAX_H, Tuple.Operation & FEC_OP_START_ENCODER);
+          FEC_MAX_H, Tuple.Operation & FEC_OP_START_ENCODER);
         }
         Offset++;
       }
-      Output = Input;
-      End = Input.End_of_frame;
+
+      Parity[Word_offset++] = Output;
     }
-    else if (Tuple.Operation & FEC_OP_GET_ENCODED)
+    while (!End);
+  }
+  else if (Tuple.Operation & FEC_OP_GET_ENCODED)
+  {
+    unsigned Word_offset = 0;
+    unsigned Input_finished = 0;
+    unsigned End = 0;
+    unsigned New_packet_length = FEC_ETH_HEADER_SIZE / 8;
+    do
     {
+#pragma HLS LOOP_TRIPCOUNT min=8 max=190
+#pragma HLS pipeline
+
+      // I applied a couple of tricks to get the clock period down to 3 ns.
+      // The loop exit condition (End) took several cycles to compute
+      // originally.  Because of that, the initiation interval was 3 instead of
+      // 1.  I found out that if-statements had a bad effect on the latency, so
+      // I replaced them with conditional expressions.  I would have expected
+      // the tool to do if-conversion if needed, but apparently that did not
+      // happen.  This change was not enough, so I broke the computation apart
+      // by calculating the packet length a cycle earlier.  That did the trick.
+
+      const packet_interface Empty = {0, 0, 0, 1, 0};
+
+      unsigned Packet_length = New_packet_length;
+
+      packet_interface Input = Input_finished ? Empty : Data[Word_offset];
+      New_packet_length += Input_finished ? 0 : Input.Count;
+
+      packet_interface Output = Input;
+
+      Input_finished = Input.End_of_frame;
+
+      End = (Word_offset == (Packet_length / 8));
+
       Output.Data = 0;
-      int Offset = 8 * Word_offset;
-      for (int Byte_offset = 0; Byte_offset < 8; Byte_offset++)
+      unsigned Offset = 8 * Word_offset;
+      for (unsigned Byte_offset = 0; Byte_offset < 8; Byte_offset++)
       {
         Output.Data <<= 8;
         if (8 * Offset < FEC_ETH_HEADER_SIZE + FEC_HEADER_SIZE)
@@ -123,15 +153,13 @@ void RSE_core(tuple_interface Tuple, packet_interface * Data,
         Offset++;
       }
 
-      End = (8 * Word_offset == Packet_length);
-
-      Output.Start_of_frame = (Word_offset == 0);
+      Output.Start_of_frame = Word_offset == 0;
       Output.End_of_frame = End;
       Output.Count = End ? Packet_length % 8 : 8;
       Output.Error = Input.Error;
-    }
 
-    Parity[Word_offset++] = Output;
+      Parity[Word_offset++] = Output;
+    }
+    while (!End);
   }
-  while (!End);
 }
