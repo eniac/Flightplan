be15bc8b08a82fc2087edc3aef0e4a0bb54adaf3
Nik Sultana <nik.sultana@cl.cam.ac.uk>
2018-03-28 09:32:13 -0400

    more prep;

--- cpuBoosters/fecEncodeBooster/fecDecodeBooster.c
+++ cpuBoosters/fecEncodeBooster/fecDecodeBooster.c
@@ -4,8 +4,15 @@
 
 #include "fecBooster.h"
 
+bool nothing_to_decode = true;
+int lastBlockId = 0;
+
 // Try to decode new packets, and forward them on.
 void decode_and_forward(const struct fec_header *fecHeader) {
+	if (nothing_to_decode) {
+		return;
+	}
+
 	// FIXME when decode is run, find out what "new" packets we have and forward them. then the buffer is made available to another block_id
 
 
@@ -22,13 +29,12 @@ void decode_and_forward(const struct fec_header *fecHeader) {
 	for (int i = NUM_DATA_PACKETS; i < NUM_DATA_PACKETS+NUM_PARITY_PACKETS; i++) {
 		char* packetToInject = pkt_buffer[fecHeader->block_id][i];
 		size_t outPktLen = get_total_packet_size(packetToInject);
-		pcap_inject(handle, packetToInject, outPktLen);
+		pcap_inject(handle, packetToInject, outPktLen); // FIXME use forwarding function
 	}
+
+	nothing_to_decode = true;
 }
 
-
-int lastBlockId = 0;
-int lastPacketId = 0;
 /**
  * @brief      packet handler function for pcap
  *
@@ -50,7 +56,6 @@ void my_packet_handler(
 		return;
 	}
 
-	// FIXME examine Wharf tag further: if next block_id started then run decode on what we have so far, see what can be retrieved.
 	if (fecHeader->block_id != lastBlockId){
 		decode_and_forward(fecHeader);
 		lastBlockId = fecHeader->block_id;
@@ -60,13 +65,12 @@ void my_packet_handler(
 	// FIXME examine Wharf tag further: if packet is a parity then buffer it; if it's a data packet then buffer it, strip the tag and forward. Strip tag before buffering (for decoding).
 
 	if (fecHeader->index < NUM_DATA_PACKETS){
-		// FIXME strip tag
+		// FIXME strip tag; and use forwarding function
 		pcap_inject(handle, packet, header->len);
 	}
 
 	/*Update the received pkt in the pkt buffer.*/
 	if (pkt_buffer_filled[fecHeader->block_id][fecHeader->index] == 0) {
-
 		memcpy(pkt_buffer[fecHeader->block_id][fecHeader->index], packet, header->len);
 		pkt_buffer_filled[fecHeader->block_id][fecHeader->index] = 1;
 	}
