caf2c787af5f7fc259f7666f2d8e6a493d1f5cf1
Nik Sultana <nik.sultana@cl.cam.ac.uk>
2018-03-29 10:13:54 -0400

    started fecTagEncodeBooster.c as an improvement of fecEncodeBooster.c, to be compatible with fecDecodeBooster.c;

---
+++ cpuBoosters/fecEncodeBooster/fecTagEncodeBooster.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "fecBooster.h"
+
+int lastBlockId = 0;
+int lastPacketId = 0;
+/**
+ * @brief      packet handler function for pcap
+ *
+ * @param      args    The arguments
+ * @param[in]  header  The header
+ * @param[in]  packet  The packet
+ *
+ * Packets in a block can be reordered, but blocks must not have their packets mixed:
+ * this code gets confused if packets of one block appear while the code is encoding another,
+ * and unless is_all_pkts_recieved_for_block()==true then the parity packets won't be sent out.
+ */
+void my_packet_handler(
+    u_char *args,
+    const struct pcap_pkthdr *header,
+    const u_char *packet
+) {
+	const struct fec_header *fecHeader = (struct fec_header *) (packet + SIZE_ETHERNET);
+
+	// skip blocks that don't belong to this worker.
+	if ((fecHeader->block_id) % workerCt != workerId){
+		return;
+	}
+
+	if (fecHeader->block_id != lastBlockId){
+		lastBlockId = fecHeader->block_id;
+		zeroout_block_in_pkt_buffer(lastBlockId);
+	} else if (fecHeader->index < lastPacketId){
+		fprintf(stderr, "fecHeader->index got smaller?\n");
+	} else if (fecHeader->index > lastPacketId + 1){
+		fprintf(stderr, "Missed a packet\n");
+	}
+	lastPacketId = fecHeader->index;
+
+	if (fecHeader->index < NUM_DATA_PACKETS){
+		forward_frame(packet, header->len);
+	}
+
+	/*Update the received pkt in the pkt buffer.*/
+	if (pkt_buffer_filled[fecHeader->block_id][fecHeader->index] == PACKET_ABSENT) {
+		memcpy(pkt_buffer[fecHeader->block_id][fecHeader->index], packet, header->len);
+		pkt_buffer_filled[fecHeader->block_id][fecHeader->index] = PACKET_PRESENT;
+	} 
+	else {
+		fprintf(stderr, "Not buffering duplicate packet\n");
+	}
+
+	/*check if the block is ready for processing*/
+	if (is_all_pkts_recieved_for_block(fecHeader->block_id) == true) {
+		/*populate the global fec structure for rse encoder and call the encode.*/
+		call_fec_blk_get(fecHeader->block_id);
+
+#ifndef FEC_ENCODE_BOOSTER_BASELINE
+		/* Encoder */
+		encode_block();
+
+		copy_parity_packets_to_pkt_buffer(fecHeader->block_id);
+#endif // FEC_ENCODE_BOOSTER_BASELINE
+
+		/*Inject all packets in the block back to the network*/
+		for (int i = NUM_DATA_PACKETS; i < NUM_DATA_PACKETS+NUM_PARITY_PACKETS; i++) {
+			char* packetToInject = pkt_buffer[fecHeader->block_id][i];
+			size_t outPktLen = get_total_packet_size(packetToInject);
+			forward_frame(packetToInject, outPktLen);
+		}
+	}
+	return;
+}
