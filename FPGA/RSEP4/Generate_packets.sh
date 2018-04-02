6716bebe9946d403d4b9d48cf0fe926d90510c00
Hans Giesen <giesen@seas.upenn.edu>
2018-04-02 18:22:32 -0400

    Replaced feedback loop implementation by one with state machines that is easier to maintain.

--- FPGA/RSEP4/Generate_packets.sh
+++ FPGA/RSEP4/Generate_packets.sh
@@ -1,19 +1,22 @@
 #!/bin/bash -e
 
+K=8
+H=4
+PACKET_COUNT=100
 PAYLOAD_LENGTH=64
 
 GENERATOR=../../../Utilities/PacketGenerator/PacketGenerator.py
 
-$GENERATOR -t text -l $PAYLOAD_LENGTH Packet.user
-$GENERATOR -t axi -f 0 -l $PAYLOAD_LENGTH Packet_feedback_in.axi
+$GENERATOR -t text -b $PACKET_COUNT -l $PAYLOAD_LENGTH -k $K -f $H Packet.user
+$GENERATOR -t axi -b $PACKET_COUNT -l $PAYLOAD_LENGTH -k $K -f 0 Packet_feedback_in.axi
 
-for i in $(seq 3)
+for i in $(seq $PACKET_COUNT)
 do
-  for j in $(seq 8)
+  for j in $(seq $K)
   do
     echo "40"
   done
-  for j in $(seq 4)
+  for j in $(seq $H)
   do
     echo "30"
   done
