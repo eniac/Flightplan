bdf44ba85426e23ca5817d7d52b306dcda2fe3cd
Nik Sultana <nik.sultana@cl.cam.ac.uk>
2018-04-10 20:10:11 -0400

    fixed compiler warnings for fecTagUntagBooster.c;

--- cpuBoosters/fecEncodeBooster/fecTagUntagBooster.c
+++ cpuBoosters/fecEncodeBooster/fecTagUntagBooster.c
@@ -12,11 +12,11 @@ void my_packet_handler(
 	u_char *new_packet = NULL;
 	int tagged_size = wharf_tag_frame(packet, header->len, &new_packet);
 	int new_size = wharf_strip_frame(new_packet, tagged_size);
-	if (new_size != header->len) {
+	if ((unsigned)new_size != header->len) {
 		fprintf(stderr, "Frame size changed\n");
 		exit(1);
 	}
-	for (int i = 0; i < header->len; i++) {
+	for (int i = 0; (unsigned)i < header->len; i++) {
 		if (new_packet[i] != packet[i]) {
 			fprintf(stderr, "Frame changed\n");
 			exit(1);
