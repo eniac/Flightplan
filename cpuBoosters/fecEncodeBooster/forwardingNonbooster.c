57626d58d073b0a1e72808eb6a629820bfaffe5f
Nik Sultana <nik.sultana@cl.cam.ac.uk>
2018-04-11 17:59:22 -0400

    WHARF_DROP_RATE is now set so a data packet is dropped in each block; added debug output;

--- cpuBoosters/fecEncodeBooster/forwardingNonbooster.c
+++ cpuBoosters/fecEncodeBooster/forwardingNonbooster.c
@@ -5,7 +5,7 @@
 
 #include "fecBooster.h"
 
-#define WHARF_DROP_RATE 10
+#define WHARF_DROP_RATE (NUM_DATA_PACKETS + NUM_PARITY_PACKETS - 1)
 int drop = WHARF_DROP_RATE;
 
 void my_packet_handler(u_char *args, const struct pcap_pkthdr *header, const u_char *packet) {
@@ -14,6 +14,9 @@ void my_packet_handler(u_char *args, const struct pcap_pkthdr *header, const u_c
 #else
 	if (0 == drop) {
 		drop = WHARF_DROP_RATE;
+#if WHARF_DEBUGGING
+		printf("dropped packet after forwarding %d\n", WHARF_DROP_RATE);
+#endif // WHARF_DEBUGGING
 	} else {
 		forward_frame(packet, header->len);
 		drop -= 1;
