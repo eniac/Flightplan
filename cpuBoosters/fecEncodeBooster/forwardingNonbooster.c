02d03c2193456f61b0e1ab0790d96e4ef0ab1956
Nik Sultana <nik.sultana@cl.cam.ac.uk>
2018-04-11 15:10:11 -0400

    added optional frame-dropping to the forwarder;

--- cpuBoosters/fecEncodeBooster/forwardingNonbooster.c
+++ cpuBoosters/fecEncodeBooster/forwardingNonbooster.c
@@ -5,6 +5,18 @@
 
 #include "fecBooster.h"
 
+#define WHARF_DROP_RATE 10
+int drop = WHARF_DROP_RATE;
+
 void my_packet_handler(u_char *args, const struct pcap_pkthdr *header, const u_char *packet) {
+#if WHARF_DROP_RATE == 0
 	forward_frame(packet, header->len);
+#else
+	if (0 == drop) {
+		drop = WHARF_DROP_RATE;
+	} else {
+		forward_frame(packet, header->len);
+		drop -= 1;
+	}
+#endif // WHARF_DROP_RATE > 0
 }
