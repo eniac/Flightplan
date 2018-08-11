113d7a942ca4201bfa63191c91a13588ffaf2edc
Zhaoyang <function047@gmail.com>
2018-08-10 21:44:25 -0400

    Script for packet testing

--- MemPacket/generate_packet.sh
+++ MemPacket/generate_packet.sh
@@ -1,6 +1,11 @@
+sudo killall tcpdump
 service memcached restart
 sudo tcpdump -i lo -w TX.pcap dst port 11211 &
 sudo tcpdump -i lo -w RX.pcap src port 11211 &
-memaslap -s 127.0.0.1:11211 -x 500 -F ./.memaslap.cnf -U 
-service memcached stop
+sleep 0.5
+memaslap -s 127.0.0.1:11211 -x 32 -F ./.memaslap.cnf -U 
+echo Wait 3s to make sure all packets are captured 
+sleep 2
 
+service memcached stop
+sudo killall tcpdump
--- MemPacket/send_packets.sh
+++
@@ -1,4 +0,0 @@
-while :
-	do
-	tcpreplay --topspeed -i enp3s0f0 set16.pcap
-done
