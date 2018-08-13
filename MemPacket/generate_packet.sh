fd3831723e10a2887d27dc6150497db860718bf1
Zhaoyang <function047@gmail.com>
2018-08-13 06:08:35 -0400

    Scripts for testing

--- MemPacket/generate_packet.sh
+++ MemPacket/generate_packet.sh
@@ -3,7 +3,7 @@ service memcached restart
 sudo tcpdump -i lo -w TX.pcap dst port 11211 &
 sudo tcpdump -i lo -w RX.pcap src port 11211 &
 sleep 0.5
-memaslap -s 127.0.0.1:11211 -x 32 -F ./.memaslap.cnf -U 
+memaslap -s 127.0.0.1:11211 -x 100 -F ./.memaslap.cnf -U 
 echo Wait 3s to make sure all packets are captured 
 sleep 2
 
