03ac960e4d4d690e58d841c02a2d9a222fcd8505
Zhaoyang <function047@gmail.com>
2018-08-29 10:11:29 -0400

    Added tuples processing

--- FPGA/MemcachedVivadoHLS/MemHLS.h
+++ FPGA/MemcachedVivadoHLS/MemHLS.h
@@ -33,6 +33,7 @@
 
 typedef ap_uint<MEM_AXI_BUS_WIDTH> Data_Word;
 typedef ap_uint<8> Byte;
+typedef ap_uint<1> MemcachedPkt;
 
 typedef struct
 {
@@ -59,6 +60,37 @@ typedef struct Cache_Memory{
 
 }Cache;
 
+typedef struct
+{
+    ap_uint<4> Egress_port;
+    ap_uint<4> Ingress_port;
+} tuple_ioports;
+
+typedef struct
+{
+    ap_uint<16> Id;
+} tuple_local_state;
+
+typedef struct
+{
+    ap_uint<32> Size;
+} tuple_Parser_extracts;
+
+typedef struct
+{
+    ap_uint<22> Control;
+} tuple_control;
+
+typedef struct
+{
+    ap_uint<1> Stateful_valid;
+} tuple_memcached_input;
+
+typedef struct
+{
+	ap_uint<1> forward;
+
+} tuple_checkcache;
 typedef struct
 {
 
@@ -113,12 +145,24 @@ typedef struct
 
 typedef struct
 {
-    tuple_hdr Hdr;
+	tuple_control Control;
+	tuple_checkcache Checkcache;
+	tuple_hdr Hdr;
+	tuple_ioports Ioports;
+    tuple_local_state Local_state;
+    tuple_Parser_extracts Parser_extracts;
+    tuple_memcached_input Memcached_input;
 } input_tuples;
 
 typedef struct
 {
-    tuple_hdr Hdr;
+	tuple_control Control;//23
+	tuple_checkcache Checkcache; //1
+	tuple_hdr Hdr; //372
+	tuple_ioports Ioports; //8
+    tuple_local_state Local_state; //16
+    tuple_Parser_extracts Parser_extracts; //32
+    tuple_memcached_input Memcached_output; //1
 } output_tuples;
 
 // For Hash Function
