9875e8186fd53a4b317ddd46ed3fd55a7afa63c1
Hans Giesen <giesen@seas.upenn.edu>
2018-09-14 15:22:48 -0400

    Added option to packet dropper tool to ensure that there are enough packets for a block to be reconstructed.

--- FPGA/Utilities/PacketDropper/PacketDropper.py
+++ FPGA/Utilities/PacketDropper/PacketDropper.py
@@ -21,6 +21,10 @@ def main():
                       default = 'text')
   parser.add_argument('-p', dest = 'dropProbability', help = 'Probability packet is dropped',
                       default = '0.1', type = float)
+  parser.add_argument('-r', dest = 'reconstructable', help = 'Ensure packets can be reconstructed.',
+                      action = 'store_true')
+  parser.add_argument('-k', dest = 'k', help = 'Data packets in a block', type = int, default = 5)
+  parser.add_argument('-n', dest = 'h', help = 'Parity packets in a block', type = int, default = 1)
   args = parser.parse_args()
 
   packets = []
@@ -28,7 +32,7 @@ def main():
   if args.inputType == 'text':
     readTextFile(args.inputFilename, packets)
 
-  packets = dropPackets(packets, args.dropProbability)
+  packets = dropPackets(packets, args.dropProbability, args.reconstructable, args.k, args.h)
 
   if args.outputType == 'text':
     writeTextFile(args.outputFilename, packets)
@@ -41,13 +45,27 @@ def readTextFile(filename, packets):
     for packet in textFile:
       packets.append(packet)
 
-def dropPackets(packets, dropProbability):
+def dropPackets(packets, dropProbability, reconstructable, k, h):
   """
   Drop packets.
   """
-  count = int((1 - dropProbability) * len(packets))
+  count = int(dropProbability * len(packets))
   indices = sorted(random.sample(range(0, len(packets)), count))
-  return [packets[i] for i in indices]
+
+  new_indices = indices[:]
+  if reconstructable:
+    prev_block = -1
+    for index in indices:
+      block = int(index / (k + h))
+      if block == prev_block:
+        missing += 1
+        if missing >= h:
+          new_indices.remove(index)
+      else:
+        missing = 1
+      prev_block = block
+
+  return [packets[i] for i in range(0, len(packets)) if i not in new_indices]
 
 def writeTextFile(filename, packets):
   """
