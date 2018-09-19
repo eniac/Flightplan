196ac8aa41e7d2b9d358594e711494a12d0ee554
Hans Giesen <giesen@seas.upenn.edu>
2018-09-19 14:23:19 -0400

    Added support for dropping a fixed number of packets per block to the packet dropper script.

--- FPGA/Utilities/PacketDropper/PacketDropper.py
+++ FPGA/Utilities/PacketDropper/PacketDropper.py
@@ -21,8 +21,11 @@ def main():
                       default = 'text')
   parser.add_argument('-p', dest = 'dropProbability', help = 'Probability packet is dropped',
                       default = '0.1', type = float)
-  parser.add_argument('-r', dest = 'reconstructable', help = 'Ensure packets can be reconstructed.',
-                      action = 'store_true')
+  parser.add_argument('-c', dest = 'dropCount', help = 'Packets dropped per block.',
+                      default = '1', type = int)
+  parser.add_argument('-d', dest = 'dropAlgorithm', choices = ['fixed', 'relative',
+                      'relativeReconstructable'], help = 'Probability packet is dropped',
+                      default = 'fixed')
   parser.add_argument('-k', dest = 'k', help = 'Data packets in a block', type = int, default = 5)
   parser.add_argument('-n', dest = 'h', help = 'Parity packets in a block', type = int, default = 1)
   args = parser.parse_args()
@@ -32,7 +35,8 @@ def main():
   if args.inputType == 'text':
     readTextFile(args.inputFilename, packets)
 
-  packets = dropPackets(packets, args.dropProbability, args.reconstructable, args.k, args.h)
+  packets = dropPackets(packets, args.dropProbability, args.dropCount, args.dropAlgorithm, args.k,
+                        args.h)
 
   if args.outputType == 'text':
     writeTextFile(args.outputFilename, packets)
@@ -45,27 +49,33 @@ def readTextFile(filename, packets):
     for packet in textFile:
       packets.append(packet)
 
-def dropPackets(packets, dropProbability, reconstructable, k, h):
+def dropPackets(packets, dropProbability, dropCount, dropAlgorithm, k, h):
   """
   Drop packets.
   """
-  count = int(dropProbability * len(packets))
-  indices = sorted(random.sample(range(0, len(packets)), count))
-
-  new_indices = indices[:]
-  if reconstructable:
-    prev_block = -1
-    for index in indices:
-      block = int(index / (k + h))
-      if block == prev_block:
-        missing += 1
-        if missing >= h:
-          new_indices.remove(index)
-      else:
-        missing = 1
-      prev_block = block
-
-  return [packets[i] for i in range(0, len(packets)) if i not in new_indices]
+  if dropAlgorithm == 'relative' or dropAlgorithm == 'relativeReconstructable':
+    count = int(dropProbability * len(packets))
+    indices = sorted(random.sample(range(len(packets)), count))
+    new_indices = indices[:]
+    if dropAlgorithm == 'relativeReconstructable':
+      prev_block = -1
+      for index in indices:
+        block = int(index / (k + h))
+        if block == prev_block:
+          missing += 1
+          if missing >= h:
+            new_indices.remove(index)
+        else:
+          missing = 1
+        prev_block = block
+  elif dropAlgorithm == 'fixed':
+    block_count = int(math.ceil(len(packets) / (k + h)))
+    new_indices = []
+    for block in range(block_count):
+      indices = random.sample(range(k + h), dropCount)
+      new_indices.extend([block * (k + h) + index for index in indices])
+    print new_indices
+  return [packets[i] for i in range(len(packets)) if i not in new_indices]
 
 def writeTextFile(filename, packets):
   """
