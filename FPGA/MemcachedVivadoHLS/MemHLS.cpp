8bc6be807ce28cdeafec27e263880271d80e6227
Isaac Pedisich <iped@seas.upenn.edu>
2018-11-12 16:01:40 -0500

    moved MemHLS_hash to its own header file

--- FPGA/MemcachedVivadoHLS/MemHLS.cpp
+++ FPGA/MemcachedVivadoHLS/MemHLS.cpp
@@ -1,4 +1,5 @@
 #include "MemHLS.h"
+#include "MemHLS_hash.h"
 #include <iostream>
 #define STORE_FROMSERVER 
 
@@ -12,24 +13,6 @@
 
 static Cache Memory[MAX_MEMORY_SIZE];
 static uint16_t Packet_num;
-uint16_t hash(Data_Word Data)
-{
-#pragma HLS inline
-	Data_Word temp = Data;
-	uint16_t result = 0;
-	Data = (~Data) + (Data << 21);
-	Data = Data ^ (Data >> 24);
-	Data = (Data + (Data << 3)) + (Data << 8);
-	Data = Data ^ (Data >> 14);
-	Data = (Data + (Data << 2)) + (Data << 4);
-	Data = Data ^ (Data >> 28);
-	Data = Data + (Data << 31);
-	result = (uint16_t) (Data % MAX_MEMORY_SIZE);
-	result ^= temp.range(16,0);
-	result %= MAX_MEMORY_SIZE;
-	return result;
-}
-
 
 uint8_t Find_delimiter(Data_Word &buffer)
 {
