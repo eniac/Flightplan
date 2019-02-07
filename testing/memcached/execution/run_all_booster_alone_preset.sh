0b3aa36352767dc407e8f37506f31261ad3f843e
Isaac Pedisich <iped@seas.upenn.edu>
2019-02-07 21:29:16 +0000

    updated run scripts to remove unused variables

--- testing/memcached/execution/run_all_booster_alone_preset.sh
+++ testing/memcached/execution/run_all_booster_alone_preset.sh
@@ -3,13 +3,10 @@ if [[ $# < 1 ]]; then
     exit 0
 fi
 
-GETPCT="get_pct:.95"
 BOOST="mcd_ip:10.0.0.10"
-MCDT="mcd_threads:8"
-MCDM="mcd_memory:4096"
 RATES="0.25,0.50,0.75,1.00,1.25,1.50,1.75,2.00,2.25,2.50,2.75,3.00"
 
 python run_rates.py $1 cfgs/booster_alone_preset.yml \
-    --args "${GETPCT};${BOOST};${MCDT};${MCDM};p_collision:0" \
+    --args "${BOOST};p_collision:0" \
     --out "../new_output/upset_booster_alone/" \
     --rates "$RATES"
