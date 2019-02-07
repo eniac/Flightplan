b0c858b44362d3fd541a415a206f0010440a0526
Isaac Pedisich <iped@seas.upenn.edu>
2019-02-07 21:23:50 +0000

    adding run scripts for memcached

---
+++ testing/memcached/execution/run_all_mcd_alone_preset.sh
@@ -0,0 +1,17 @@
+if [[ $# < 1 ]]; then
+    echo "Usage $0 <label>"
+    exit 0
+fi
+
+GETPCT="get_pct:.95"
+BOOST="mcd_ip:10.0.0.20"
+MCDT="mcd_threads:8"
+MCDM="mcd_memory:4096"
+RATES="0.01,0.02,0.05,0.10,0.15,0.20,0.25,0.30,0.35,0.40,0.45,0.50"
+#RATES="0.30,0.35"
+#RATES="0.25,0.50,0.75,1.00,1.25,1.50,1.75,2.00,2.25,2.50,2.75,3.00"
+
+python run_rates.py $1 cfgs/mcd_alone_preset.yml \
+    --args "${GETPCT};${BOOST};${MCDT};${MCDM};p_collision:0" \
+    --out "../new_output/upset_mcd_alone/" \
+    --rates "$RATES"
