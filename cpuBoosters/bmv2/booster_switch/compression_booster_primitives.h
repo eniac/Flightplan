a0250291ee70487ecb530067e109da65ee354cff
Isaac Pedisich <iped@seas.upenn.edu>
2019-03-21 15:39:01 +0000

    added compression booster primitives in source and makefile

---
+++ cpuBoosters/bmv2/booster_switch/compression_booster_primitives.h
@@ -0,0 +1,6 @@
+#ifndef BOOSTER_SWITCH__COMPRESSION_BOOSTER_PRIMTIVES_H_
+#define BOOSTER_SWITCH__COMPRESSION_BOOSTER_PRIMTIVES_H_
+
+int import_compression_booster_primitives(SimpleSwitch *sswitch);
+
+#endif  // BOOSTER_SWITCH__FEC
