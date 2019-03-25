6fad8ba496c28c167528dcf46311c6d7a69ee1d2
Isaac Pedisich <iped@seas.upenn.edu>
2019-03-25 14:50:26 +0000

    host program can be specified from command line
    Also added a --time argument, and added more print statements to stdout
    that describe the execution of start_flightplan_mininet.py

--- Wharf/bmv2/start_flightplan_mininet.py
+++ Wharf/bmv2/start_flightplan_mininet.py
@@ -52,8 +52,10 @@ parser.add_argument('--bmv2-exe', help='Path to bmv2 executable',
                     type=str, required=False, default=None)
 parser.add_argument('--replay', help='Provide a pcap file to be sent through from h1 to h2',
                     type=str, action='store', required=False, default=False)
-
-
+parser.add_argument('--host-prog', help='Run a program on a host. Syntax = "hostname:program to run"',
+                    type=str, action='append', required=False, default=[])
+parser.add_argument('--time', help='Time to run mininet for',
+                    type=int, required=False, default=1)
 # Modified below in main execution
 # (sorry for the global)
 global cfg_file
@@ -98,6 +100,7 @@ class FPTopo(Topo):
 
 
             link_names.extend([(sw_name, link) for link in sw_opts.get('links', [])])
+            print("SWITCH: %s" % self.all_nodes[sw_name])
 
         for i, host in enumerate(sorted(host_spec)):
             self.all_nodes[host] = dict(
@@ -108,7 +111,7 @@ class FPTopo(Topo):
                     ip = '10.0.{}.1'.format(i),
                     mac = '00:04:00:00:00:{:02x}'.format(i),
             )
-            print("HOSTS",self.all_nodes[host])
+            print("HOST: %s" % self.all_nodes[host])
 
             #nodes[host].setDefaultRoute('dev eth0 via 10.0.%d.1' %i)
 
@@ -147,10 +150,15 @@ class FPTopo(Topo):
 
     def start_host_dump(self, net, directory):
         for host in self.host_spec:
-            net.get(host).cmd('tcpdump -i eth0 -Q out -w {}&'.format(os.path.join(directory, host + '_out.pcap')))
-            net.get(host).cmd('tcpdump -i eth0 -Q in -w {}&'.format(os.path.join(directory, host + '_in.pcap')))
+            fname = os.path.join(directory, host + '_out.pcap')
+            print("Dumping {} output to {}".format(host, fname))
+            net.get(host).cmd('tcpdump -i eth0 -Q out -w {}&'.format(fname))
+            fname = os.path.join(directory, host + '_in.pcap')
+            print("Dumping {} input to {}".format(host, fname))
+            net.get(host).cmd('tcpdump -i eth0 -Q in -w {}&'.format(fname))
 
     def stop_host_dump(self, net):
+        print("Stopping tcpdump on hosts")
         for host in self.host_spec:
             net.get(host).cmd('pkill tcpdump')
 
@@ -158,6 +166,7 @@ class FPTopo(Topo):
     def do_switch_replay(self, net):
         for sw1_name, sw_opts in self.switch_spec.items():
             for sw2_name, filename in sw_opts.get('replay',{}).items():
+                print("Replaying {} from {} to {}".format(filename, sw1_name, sw2_name))
                 num = self.all_links['{}-{}'.format(sw1_name, sw2_name)]
                 net.get(sw1_name).cmd(
                         'tcpreplay -i {}-eth{} {}'.format(sw1_name, num, cfgpath(filename))
@@ -167,17 +176,27 @@ class FPTopo(Topo):
     def do_commands(self, net):
         for sw_name, sw_opts in self.switch_spec.items():
             if 'cmds' in sw_opts:
-                commands = open(cfgpath(sw_opts['cmds'])).readlines()
-                send_commands(self.all_nodes[sw_name]['port'], cfgpath(sw_opts['cfg']), commands)
+                for cmd_file in sw_opts['cmds']:
+                    commands = open(cfgpath(cmd_file)).readlines()
+                    send_commands(self.all_nodes[sw_name]['port'], cfgpath(sw_opts['cfg']), commands)
 
-    def run_host_programs(self, net):
+    def run_host_programs(self, net, extras):
         for name, spec in self.host_spec.items():
             if 'program' in spec:
+                print("Running {} on {}".format(spec['program'], name))
                 net.get(name).cmd('{} > {}/{}_prog.log 2>&1 &'.format(spec['program'], self.log_dir, name))
-                sleep(10)
+
+        for extra_prog in extras:
+            try:
+                name, program = extra_prog.split(':')
+            except:
+                print("Programs provided from cli must be of form 'h1:program to run'")
+                raise
+            print("Running {} on {}".format(program, name))
+            net.get(name).cmd('{} > {}/{}_prog.log 2>&1 &'.format(program, self.log_dir, name))
 
     def do_host_replay(self, net, host, towards, file):
-        net.get(host).cmd('ifconfig')
+        print("Replaying {} on {}.eth0".format(file, host))
         net.get(host).cmd(
                 'tcpreplay -p 100 -i eth0 {}'.format(file)
         )
@@ -199,6 +218,7 @@ def main():
     topo = FPTopo(cfg['hosts'], cfg['switches'],
                   bmv2_exe, args.log, args.verbose, args.pcap_dump)
 
+    print("Starting mininet")
     net = Mininet(topo=topo, host=P4Host, switch=P4Switch, controller=None)
 
     net.start()
@@ -217,7 +237,7 @@ def main():
 
     topo.do_commands(net)
 
-    topo.run_host_programs(net)
+    topo.run_host_programs(net, args.host_prog)
 
     if args.replay:
         replay_args = args.replay.split(":")
@@ -225,13 +245,16 @@ def main():
         if len(replay_args) != 2 or len(replay_arg1) != 2:
             raise Exception("args.replay must have form Host-Switch:File")
         topo.do_host_replay(net, replay_arg1[0], replay_arg1[1], replay_args[1])
-        sleep(2)
+        sleep(1)
+
+    sleep(args.time)
 
     if args.cli:
         CLI(net)
 
     if args.pcap_dump:
         topo.stop_host_dump(net)
+    print("Stoping mininet")
 
 if __name__ == '__main__':
     setLogLevel('debug')
