9531b5180b9e3227b96e62f779f8fcb3ac1d277d
Isaac Pedisich <iped@seas.upenn.edu>
2019-04-15 12:22:28 -0400

    TOPO was not being used when specifying no header compression

--- Wharf/bmv2/complete_fec_e2e.sh
+++ Wharf/bmv2/complete_fec_e2e.sh
@@ -38,7 +38,7 @@ else
 fi
 
 sudo -E python $HERE/start_flightplan_mininet.py \
-        $HERE/topologies/complete_topology.yml \
+        $TOPO \
         --pcap-dump $PCAP_DUMPS \
         --log $LOG_DUMPS \
         --verbose \
@@ -98,7 +98,7 @@ if [[ $INLINES == $OUTLINES ]]; then
         echo -e ${GREEN}TEST SUCCEEDED${NC}
 
         echo Bytes Transferred:
-        python $HERE/pcap_tools/pcap_size.py \
+        python2 $HERE/pcap_tools/pcap_size.py \
             $PCAP_DUMPS/{h1_to_s1,s1_to_s2,s2_to_s3,s3_to_h2}.pcap
 
         exit 0
