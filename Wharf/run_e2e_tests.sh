152e54cd47e1757671fc90c4df05af1291b4740e
Nik Sultana <nik.sultana@cl.cam.ac.uk>
2019-04-18 14:48:23 -0400

    added two-halves experiment to the rest of the tests;

--- Wharf/run_e2e_tests.sh
+++ Wharf/run_e2e_tests.sh
@@ -7,6 +7,15 @@ else
     echo "SUCCESS";
 fi
 
+echo "***** RUNNING FEC E2E (TWO_HALVES) *****"
+TWO_HALVES=1 ./bmv2/complete_fec_e2e.sh bmv2/pcaps/tcp_100.pcap
+
+if [[ $? != 0 ]]; then
+    echo "FAILED";
+else
+    echo "SUCCESS";
+fi
+
 echo "***** RUNNING MCD E2E *****"
 ./bmv2/complete_mcd_e2e.sh bmv2/pcaps/Memcached_in_short.pcap bmv2/pcaps/Memcached_expected_short.pcap
 
