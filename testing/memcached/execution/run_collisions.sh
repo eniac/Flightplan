2ce9b7d8c1eddef77b31718f94aa11630f5f869a
Isaac Pedisich <iped@seas.upenn.edu>
2019-05-01 17:52:49 +0000

    transitioning to new memcached experiments

---
+++ testing/memcached/execution/run_collisions.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+OUT=$1
+LABEL=$2
+ZIPF=$3
+RATES=$4
+
+if [[ $LABEL == "" || $OUT == "" ]]; then
+    echo "usage $0 OUT LABEL ZIPF RATES"
+    exit -1;
+fi
+
+if [[ $ZIPF == "" ]]; then
+    echo "Setting ZIPF=0.9";
+    ZIPF=0.9
+fi
+
+if [[ $RATES == "" ]]; then
+    RATES="0.10 0.50 1.00 $(seq -f "%.02f" 2 2 30)";
+fi
+
+mkdir -p $OUT/$LABEL
+
+run_booster () {
+    python ../../Shremote/shremote.py cfgs/tofino_moongen.yml ${LABEL}_$1 --out $OUT/$LABEL --args="rate:$1;p_collision:0.1;dataplane_flags:-k;gen_mcd_flags:--zipf-random $ZIPF" --delete;
+    RTN=$?
+    RETRIES=1
+    while [[ $RTN != 0 ]]; do
+        echo "Trying again... $RETRIES"
+        sleep 5
+        python ../../Shremote/shremote.py cfgs/tofino_moongen.yml ${LABEL}_$1 --out $OUT/$LABEL --args="rate:$1;dataplane_flags:-k" --delete;
+        RTN=$?
+        RETRIES=$(( $RETRIES + 1 ))
+    done
+    echo "SUCCESS!";
+    sleep 5
+}
+
+for rate in $RATES; do
+    run_booster $rate;
+done
---
+++ testing/memcached/execution/verify_counters.py
@@ -0,0 +1,37 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("moongen_log")
+parser.add_argument("counter_1")
+parser.add_argument("counter_2")
+
+args = parser.parse_args()
+
+
+count_file = args.counter_1
+for line in open(count_file):
+    if line.startswith('5/0'):
+        n_pkts_str = line.split()[-1]
+        n_start_pkts = int(n_pkts_str[:-1])
+count_file = args.counter_2
+for line in open(count_file):
+    if line.startswith('5/0'):
+        n_pkts_str = line.split()[-1]
+        n_end_pkts = int(n_pkts_str[:-1])
+n_expected_pkts = (n_end_pkts - n_start_pkts)
+
+last_line = open(args.moongen_log).readlines()[-1]
+split_line = last_line.split()
+
+for word, next_word in zip(split_line, split_line[1:]):
+    if word == 'total':
+        n_got = int(next_word)
+
+print("Expected: {}\nGot: {}".format(n_expected_pkts, n_got))
+print("Difference: {}".format(n_expected_pkts - n_got))
+
+if n_expected_pkts != n_got:
+    exit(n_expected_pkts - n_got)
+if n_got == 0:
+    exit(-1)
+exit(0)
