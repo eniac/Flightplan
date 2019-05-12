dac712b0a5e4ae0bbf30c7dcfb1a4e2a155a1747
Nik Sultana <nik.sultana@cl.cam.ac.uk>
2019-05-12 15:19:19 -0400

    added sketch of new extern object used in CheckedFragment;

---
+++ cpuBoosters/bmv2/booster_switch/SenderSeqState.cpp
@@ -0,0 +1,40 @@
+#include "booster_primitives.hpp"
+#include <bm/bm_sim/extern.h>
+#include <bm/bm_sim/P4Objects.h>
+#include <bm/bm_sim/logger.h>
+using namespace bm;
+
+class SenderSeqState : public ExternType {
+ public:
+
+  /** These attributes should be settable in the SenderSeqState constructor */
+  BM_EXTERN_ATTRIBUTES {
+    BM_EXTERN_ATTRIBUTE_ADD(seq);
+  }
+
+  /** Called on construction, after attributes have been set */
+  void init() override {
+    seq_ = seq.get<int>();
+    BMLOG_DEBUG("Initialized SenderSeqState with seq: {}", seq_);
+  }
+
+  void nextSeq(Data &next_seq) {
+    seq_++;
+    next_seq.set(seq_);
+  }
+
+ private:
+  // declared attributes
+  Data seq{0};
+
+  // implementation members
+  int seq_;
+};
+
+BM_REGISTER_EXTERN(SenderSeqState);
+BM_REGISTER_EXTERN_METHOD(SenderSeqState, nextSeq, Data &);
+
+/** This function stops the linker from discarding this file*/
+void import_sample_extern_object(SimpleSwitch *sswitch) {
+    (void)sswitch; // Avoids unused warning
+}
