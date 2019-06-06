db192e9d4909214af7f83c3723fb3a71819b3af9
Nik Sultana <nik.sultana@cl.cam.ac.uk>
2019-06-06 15:39:57 -0400

    putting regression test output into separate directory;

--- Wharf/regression_tests.sh
+++ Wharf/regression_tests.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+mkdir -p regression_test_output
+
 echo "FEC_E2E"
-./bmv2/complete_fec_e2e.sh bmv2/pcaps/tcp_100.pcap > FEC_E2E.stdout 2> FEC_E2E.stderr
+./bmv2/complete_fec_e2e.sh bmv2/pcaps/tcp_100.pcap > regression_test_output/FEC_E2E.stdout 2> regression_test_output/FEC_E2E.stderr
 if [ $? -eq 0 ]; then
     echo "FAILED";
 else
@@ -9,7 +11,7 @@ else
 fi
 
 echo "FEC_E2E_2-1 (TWO_HALVES=1)"
-TWO_HALVES=1 ./bmv2/complete_fec_e2e.sh bmv2/pcaps/tcp_100.pcap > FEC_E2E_2-1.stdout 2> FEC_E2E_2-1.stderr
+TWO_HALVES=1 ./bmv2/complete_fec_e2e.sh bmv2/pcaps/tcp_100.pcap > regression_test_output/FEC_E2E_2-1.stdout 2> regression_test_output/FEC_E2E_2-1.stderr
 if [ $? -eq 0 ]; then
     echo "FAILED";
 else
@@ -17,7 +19,7 @@ else
 fi
 
 echo "FEC_E2E_2-2 (TWO_HALVES=2)"
-TWO_HALVES=2 ./bmv2/complete_fec_e2e.sh bmv2/pcaps/tcp_100.pcap > FEC_E2E_2-2.stdout 2> FEC_E2E_2-2.stderr
+TWO_HALVES=2 ./bmv2/complete_fec_e2e.sh bmv2/pcaps/tcp_100.pcap > regression_test_output/FEC_E2E_2-2.stdout 2> regression_test_output/FEC_E2E_2-2.stderr
 if [ $? -eq 0 ]; then
     echo "FAILED";
 else
@@ -25,7 +27,7 @@ else
 fi
 
 echo "MCD_E2E"
-./bmv2/complete_mcd_e2e.sh bmv2/pcaps/Memcached_in_short.pcap bmv2/pcaps/Memcached_expected_short.pcap > MCD_E2E.stdout 2> MCD_E2E.stderr
+./bmv2/complete_mcd_e2e.sh bmv2/pcaps/Memcached_in_short.pcap bmv2/pcaps/Memcached_expected_short.pcap > regression_test_output/MCD_E2E.stdout 2> regression_test_output/MCD_E2E.stderr
 if [ $? -eq 0 ]; then
     echo "FAILED";
 else
@@ -33,7 +35,7 @@ else
 fi
 
 echo "HC_E2E"
-./bmv2/compressor_e2e.sh bmv2/pcaps/collidingFlows.pcap > HC_E2E.stdout 2> HC_E2E.stderr
+./bmv2/compressor_e2e.sh bmv2/pcaps/collidingFlows.pcap > regression_test_output/HC_E2E.stdout 2> regression_test_output/HC_E2E.stderr
 if [ $? -eq 0 ]; then
     echo "FAILED";
 else
