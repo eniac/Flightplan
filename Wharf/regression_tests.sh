aa5548f2c197050a8c49ae3367221f6ed688b8a0
Nik Sultana <nik.sultana@cl.cam.ac.uk>
2019-06-06 16:19:51 -0400

    compacted script;

--- Wharf/regression_tests.sh
+++ Wharf/regression_tests.sh
@@ -2,42 +2,21 @@
 
 mkdir -p regression_test_output
 
-echo "FEC_E2E"
-./bmv2/complete_fec_e2e.sh bmv2/pcaps/tcp_100.pcap > regression_test_output/FEC_E2E.stdout 2> regression_test_output/FEC_E2E.stderr
-if [ $? -eq 0 ]; then
-    echo "SUCCESS";
-else
-    echo "FAILED";
-fi
+do_test() {
+  TITLE=$1
+  FILENAME=$2
+  CMD=$3
+  echo "${TITLE}"
+  ${CMD} > regression_test_output/${FILENAME}.stdout 2> regression_test_output/${FILENAME}.stderr
+  if [ $? -eq 0 ]; then
+      echo "SUCCESS";
+  else
+      echo "FAILED";
+  fi
+}
 
-echo "FEC_E2E_2-1 (TWO_HALVES=1)"
-TWO_HALVES=1 ./bmv2/complete_fec_e2e.sh bmv2/pcaps/tcp_100.pcap > regression_test_output/FEC_E2E_2-1.stdout 2> regression_test_output/FEC_E2E_2-1.stderr
-if [ $? -eq 0 ]; then
-    echo "SUCCESS";
-else
-    echo "FAILED";
-fi
-
-echo "FEC_E2E_2-2 (TWO_HALVES=2)"
-TWO_HALVES=2 ./bmv2/complete_fec_e2e.sh bmv2/pcaps/tcp_100.pcap > regression_test_output/FEC_E2E_2-2.stdout 2> regression_test_output/FEC_E2E_2-2.stderr
-if [ $? -eq 0 ]; then
-    echo "SUCCESS";
-else
-    echo "FAILED";
-fi
-
-echo "MCD_E2E"
-./bmv2/complete_mcd_e2e.sh bmv2/pcaps/Memcached_in_short.pcap bmv2/pcaps/Memcached_expected_short.pcap > regression_test_output/MCD_E2E.stdout 2> regression_test_output/MCD_E2E.stderr
-if [ $? -eq 0 ]; then
-    echo "SUCCESS";
-else
-    echo "FAILED";
-fi
-
-echo "HC_E2E"
-./bmv2/compressor_e2e.sh bmv2/pcaps/collidingFlows.pcap > regression_test_output/HC_E2E.stdout 2> regression_test_output/HC_E2E.stderr
-if [ $? -eq 0 ]; then
-    echo "SUCCESS";
-else
-    echo "FAILED";
-fi
+do_test "FEC_E2E" "FEC_E2E" "./bmv2/complete_fec_e2e.sh bmv2/pcaps/tcp_100.pcap"
+do_test "FEC_E2E_2-1 (TWO_HALVES=1)" "FEC_E2E_2-1" "TWO_HALVES=1 ./bmv2/complete_fec_e2e.sh bmv2/pcaps/tcp_100.pcap"
+do_test "FEC_E2E_2-2 (TWO_HALVES=2)" "FEC_E2E_2-2" "TWO_HALVES=2 ./bmv2/complete_fec_e2e.sh bmv2/pcaps/tcp_100.pcap"
+do_test "MCD_E2E" "MCD_E2E" "./bmv2/complete_mcd_e2e.sh bmv2/pcaps/Memcached_in_short.pcap bmv2/pcaps/Memcached_expected_short.pcap"
+do_test "HC_E2E" "HC_E2E" "./bmv2/compressor_e2e.sh bmv2/pcaps/collidingFlows.pcap"
