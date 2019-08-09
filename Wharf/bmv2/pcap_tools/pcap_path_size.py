59f33372d167807852cfac59a83e2778eb4aed9a
Isaac Pedisich <iped@seas.upenn.edu>
2019-08-09 16:23:37 +0000

    path_size.py uses dpkt instead of scapy

--- Wharf/bmv2/pcap_tools/pcap_path_size.py
+++ Wharf/bmv2/pcap_tools/pcap_path_size.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 from scapy.all import *
+import dpkt
 import yaml
 import sys
 from collections import defaultdict
@@ -75,7 +76,9 @@ def show_size(n1, n2, sizes, out_dir):
     size = 0
     n = 0
 
-    for pkt in rdpcap(open(filename, 'rb')):
+    f = open(filename, 'rb')
+    pcap=dpkt.pcap.Reader(f)
+    for _, pkt in pcap:
         size += len(pkt)
         n += 1
 
