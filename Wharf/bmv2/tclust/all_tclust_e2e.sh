b203ec16c8af70b55e9c639e48eb61340c682a20
Isaac Pedisich <iped@seas.upenn.edu>
2019-08-12 14:04:10 +0000

    added bw arg to all_tclust_e2e.sh

--- Wharf/bmv2/tclust/all_tclust_e2e.sh
+++ Wharf/bmv2/tclust/all_tclust_e2e.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 HERE=`dirname $0`
+BW=$1
 
 run_exp() {
-    $HERE/tclust_e2e.sh $HERE/../topologies/tclust/$1;
+    $HERE/tclust_e2e.sh $HERE/../topologies/tclust/$1 $BW;
 }
 
 run_exp tclust_noop.yml
