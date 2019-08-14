75850d00d60e5c2f30148db2bfcef6dc60e816ca
Nishanth <ncshy@seas.upenn.edu>
2019-08-14 20:41:02 +0000

    Updated Benchmark table with Snort values

--- testing/fec/execution/run_lua.sh
+++ testing/fec/execution/run_lua.sh
@@ -12,4 +12,4 @@ cd $PKTGEN_DIR
 # -l <list of cores to use>
 # -n <number of memory channels (always for for dell 720s)
 # -m [cores for rx:cores for tx].[device #s to use (0 for first NIC)]
-./app/x86_64-native-linuxapp-gcc/pktgen -l 0,1,2,3,4,5,6,7 -n 2 -w 08:00.1 -- -P -f $LUAFILE -m "[1-6:7].[0]" -s 0:$PCAPFILE 
+./app/x86_64-native-linuxapp-gcc/pktgen -l 0,1,2 -n 2 -w 08:00.1 -- -P -f $LUAFILE -m "[1:2].[0]" -s 0:$PCAPFILE 
