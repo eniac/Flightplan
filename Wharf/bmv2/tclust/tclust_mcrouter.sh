ec820519a8e1b7c8e4c21a72b4c49b46e1d25fec
Isaac Pedisich <iped@seas.upenn.edu>
2019-08-15 22:57:11 +0000

    background traffic and (more) accurate failure case in tclust_mcrouter.sh

--- Wharf/bmv2/tclust/tclust_mcrouter.sh
+++ Wharf/bmv2/tclust/tclust_mcrouter.sh
@@ -31,16 +31,34 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
+BGSRC_IP="10.0.0.1"
+BGSRC_MAC="24:8a:07:8f:eb:00"
+BGDST_IP="10.0.0.2"
+BGDST_MAC="24:8a:07:5b:15:35"
+
+### Need to send packets in the background so that FEC will forward packets through
+# TODO: should use timeout instead of forcing packets through with replays
+BGINPUT=$HERE/pcaps/tcp_100.pcap
+BGINPUT_REWRITTEN=$OUTDIR/bg_traffic.pcap
+python2 $HERE/pcap_tools/pcap_sub.py $BGINPUT $BGINPUT_REWRITTEN \
+    --sip=$BGSRC_IP --dip=$BGDST_IP --smac=$BGSRC_MAC --dmac=$BGDST_MAC
+
+
 FAILURE="${RED}TEST FAILED${NC}"
 SUCCESS="${GREEN}TEST SUCCEEDED${NC}"
 sudo mn -c 2> $LOG_DUMPS/mininet_clean.err
+
+N_SETS=50
+N_GETS=60
 
 sudo -E python $HERE/start_flightplan_mininet.py \
         $TOPO \
         --pcap-dump $PCAP_DUMPS \
         --log $LOG_DUMPS \
         --verbose \
-        --fg-host-prog "mcd_c:memtier_benchmark -s 10.0.0.101 -p 11211 -n 100 -c 1 -t 1  --hide-histogram -P memcache_text" \
+        --host-prog "iperf_c:tcpreplay -i iperf_c-eth0 -p 5 $BGINPUT_REWRITTEN" \
+        --fg-host-prog "mcd_c:sleep 2 && memtier_benchmark -s 10.0.0.101 -p 11211 -n $N_SETS -c 1 -t 1  --hide-histogram -P memcache_text --ratio 1:0 --key-pattern=S:S" \
+        --fg-host-prog "mcd_c:memtier_benchmark -s 10.0.0.101 -p 11211 -n $N_GETS -c 1 -t 1  --hide-histogram -P memcache_text --ratio 0:1 --key-pattern=S:S" \
         --time 1 2> $LOG_DUMPS/flightplan_mininet_log.err
 
 RTN=$?
@@ -59,4 +77,29 @@ if [[ $RTN != 0 ]]; then
     exit -1;
 fi
 
+# STORED packets are all unique, so have to --include-tcp when counting responses
+# (shouldn't be duplicates, since no drops between mcrouter and client)
+N_BACK=`python $HERE/pcap_tools/pcap_count_unique.py $PCAP_DUMPS/tofino1_to_mcd_c.pcap --include-tcp`
+N_MCD=`python $HERE/pcap_tools/pcap_count_unique.py $PCAP_DUMPS/tofino2_to_mcd_s.pcap`
 
+if [[ $N_BACK != $(( $N_SETS + $N_GETS)) ]]; then
+    ERROR="Number of returned psh packets incorrect ( $N_BACK != $(( $N_SETS + $N_GETS )) )"
+else
+    echo "Verified $N_BACK data packets sent to mcd_c"
+fi
+
+N_EXPECTED=$(( $N_GETS > $N_SETS ? $N_GETS : $N_SETS ))
+
+if [[ $N_MCD != $N_EXPECTED ]]; then
+    ERROR+="Number of packets delivered to mcd incorrect ( $N_MCD != $N_EXPECTED )"
+else
+    echo "Verified $N_MCD data packets sent to mcd_s"
+fi
+
+if [[ $ERROR != "" ]]; then
+    echo "Error: $ERROR" >&2
+    echo -e $FAILURE >&2
+    exit -1;
+fi
+
+echo -e $SUCCESS >& 2
