aeccfff00f2e5f31f420622d1720a81adfefddfd
Nik Sultana <nik.sultana@cl.cam.ac.uk>
2020-03-07 02:11:35 +0000

    added longer test;

--- Wharf/splits/ALV_split1/tests.sh
+++ Wharf/splits/ALV_split1/tests.sh
@@ -5,7 +5,7 @@
 # FIXME various hardcoded paths
 
 TOPOLOGY=splits/ALV_split1/alv_k=4.yml
-MODES=(demo1 autotest1 autotest2 autotest2B)
+MODES=(demo1 autotest1 autotest2 autotest2B autotest_long)
 
 if [ -z "${MODE}" ]
 then
@@ -339,6 +339,45 @@ function autotest2B {
      --fg-host-prog "p3h3: iperf3 -t 2 -O 1 -c 192.3.0.3" \
      --fg-host-prog "p3h3: iperf3 -t 2 -O 1 -c 192.3.1.2" \
      --fg-host-prog "p3h3: iperf3 -t 2 -O 1 -c 192.3.1.3" \
+     --fg-host-prog ": /home/nsultana/2/P4Boosters/Wharf/splits/ALV_split1/end.sh" \
+          2> $LOG_DUMPS/flightplan_mininet_log.err
+}
+
+function autotest_long {
+  sudo -E python bmv2/start_flightplan_mininet.py ${TOPOLOGY} \
+          --pcap-dump $PCAP_DUMPS \
+          --log $LOG_DUMPS \
+          --verbose \
+          --showExitStatus \
+     --host-prog "p0h0: iperf3 -s -B 192.0.0.2" \
+     --host-prog "p0h1: iperf3 -s -B 192.0.0.3" \
+     --host-prog "p0h2: iperf3 -s -B 192.0.1.2" \
+     --host-prog "p0h3: iperf3 -s -B 192.0.1.3" \
+     --host-prog "p1h0: iperf3 -s -B 192.1.0.2" \
+     --host-prog "p1h1: iperf3 -s -B 192.1.0.3" \
+     --host-prog "p1h2: iperf3 -s -B 192.1.1.2" \
+     --host-prog "p1h3: iperf3 -s -B 192.1.1.3" \
+     --host-prog "p2h0: iperf3 -s -B 192.2.0.2" \
+     --host-prog "p2h1: iperf3 -s -B 192.2.0.3" \
+     --host-prog "p2h2: iperf3 -s -B 192.2.1.2" \
+     --host-prog "p2h3: iperf3 -s -B 192.2.1.3" \
+     --host-prog "p3h0: iperf3 -s -B 192.3.0.2" \
+     --host-prog "p3h1: iperf3 -s -B 192.3.0.3" \
+     --host-prog "p3h2: iperf3 -s -B 192.3.1.2" \
+     --host-prog "p3h3: iperf3 -s -B 192.3.1.3" \
+     --fg-host-prog "p0h0: ifconfig" \
+     --fg-host-prog "p3h3: ifconfig" \
+     --fg-host-prog "p1h0: ifconfig" \
+     --fg-host-prog "p2h0: ifconfig" \
+     --fg-host-prog ": /home/nsultana/2/P4Boosters/Wharf/splits/ALV_split1/start3.sh" \
+     --fg-host-prog ": sleep 1" \
+     --fg-host-prog "p0h0: iperf3 -t 600 -O 1 -c 192.3.1.3 &" \
+     --fg-host-prog "p1h0: iperf3 -t 600 -O 1 -c 192.2.0.2 &" \
+     --fg-host-prog ": sleep 630" \
+     --fg-host-prog "p0h0: ifconfig" \
+     --fg-host-prog "p3h3: ifconfig" \
+     --fg-host-prog "p1h0: ifconfig" \
+     --fg-host-prog "p2h0: ifconfig" \
      --fg-host-prog ": /home/nsultana/2/P4Boosters/Wharf/splits/ALV_split1/end.sh" \
           2> $LOG_DUMPS/flightplan_mininet_log.err
 }
