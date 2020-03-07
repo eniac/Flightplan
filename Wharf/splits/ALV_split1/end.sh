6c4dd3938004f5e971ceec1cfa4179a02af86097
Nik Sultana <nik.sultana@cl.cam.ac.uk>
2020-03-07 01:44:06 +0000

    added TCP-based test for first split;

---
+++ Wharf/splits/ALV_split1/end.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#Test setup for Flightplan
+#Nik Sultana, UPenn, March 2020
+set -e
+
+source /home/nsultana/2/P4Boosters/Wharf/splits/ALV_split1/envars.sh
+
+${FPControl} ${TOPOLOGY} ${FPCD} get_state
+${FPControl} ${TOPOLOGY} ${FPCD} get_pip_state
+${FPControl} ${TOPOLOGY} ${FPCD} get_drop_outgoing --switch p0e0 --next_segment 2
+${FPControl} ${TOPOLOGY} ${FPCD} get_count_ack_relinks --switch p0e0 --next_segment 2
