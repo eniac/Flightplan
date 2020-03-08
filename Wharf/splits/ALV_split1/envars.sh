44472d986bd0eae36309c36b2fcad9984e407db5
Nik Sultana <nik.sultana@cl.cam.ac.uk>
2020-03-08 15:01:05 +0000

    removed some hardcoded paths;

--- Wharf/splits/ALV_split1/envars.sh
+++ Wharf/splits/ALV_split1/envars.sh
@@ -4,5 +4,3 @@
 export FPControl=/home/nsultana/2/P4Boosters/Wharf/FPControl.py
 export TOPOLOGY=/home/nsultana/2/P4Boosters/Wharf/splits/ALV_split1/alv_k\=4.yml
 export FPCD=/home/nsultana/2/P4Boosters/Wharf/splits/ALV_split1/FPControlData.yml
-
-export BMV2_REPO=/home/iped/dcomp/behavioral-model/
