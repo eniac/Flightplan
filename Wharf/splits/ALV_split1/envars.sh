e67f8e81a9643b09a27329afdb230b7567c2a905
Nik Sultana <nik.sultana@cl.cam.ac.uk>
2020-03-10 20:41:36 +0000

    renamed;

--- Wharf/splits/ALV_split1/envars.sh
+++ Wharf/splits/ALV_split1/envars.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #Test setup for Flightplan
 #Nik Sultana, UPenn, March 2020
-export FPControl=/home/nsultana/2/P4Boosters/Wharf/FPControl.py
+export FPControl=/home/nsultana/2/P4Boosters/Wharf/fpctl.py
 export TOPOLOGY=/home/nsultana/2/P4Boosters/Wharf/splits/ALV_split1/alv_k\=4.yml
 export FPCD=/home/nsultana/2/P4Boosters/Wharf/splits/ALV_split1/FPControlData.yml
--- Wharf/splits/ALV_split2/envars.sh
+++ Wharf/splits/ALV_split2/envars.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #Test setup for Flightplan
 #Nik Sultana, UPenn, March 2020
-export FPControl=/home/nsultana/2/P4Boosters/Wharf/FPControl.py
+export FPControl=/home/nsultana/2/P4Boosters/Wharf/fpctl.py
 export TOPOLOGY=/home/nsultana/2/P4Boosters/Wharf/splits/ALV_split2/alv_k\=4.yml
 export FPCD2=/home/nsultana/2/P4Boosters/Wharf/splits/ALV_split2/FPControlData.yml
