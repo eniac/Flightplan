ab9134de96717937c5fb6f6b7e23d39d82583e05
Nik Sultana <nik.sultana@cl.cam.ac.uk>
2020-03-21 23:28:46 +0000

    added new examples of V3 based on new HL3, following 7747a9df76ac44fd0b8bef28371dda5ab7f5e337;

---
+++ Wharf/splits2/ALV_Complete_2_hl3new/envars.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#Test setup for Flightplan
+#Nik Sultana, UPenn, March 2020
+export FPControl=/home/nsultana/2/P4Boosters/Wharf/fpctl.py
+export FPCD=/home/nsultana/2/P4Boosters/Wharf/splits2/ALV_Complete_2_hl3new/FPControlData.yml
+
+if [ -z "${TOPOLOGY}" ]
+then
+  echo "Need to define TOPOLOGY environment variable"
+  exit 1
+fi
---
+++ Wharf/splits2/ALV_Complete_2_hl3new/start2.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#Test setup for Flightplan
+#Nik Sultana, UPenn, March 2020
+set -e
+
+echo "Starting ALV_Complete_2 (hl3new)"
+
+source /home/nsultana/2/P4Boosters/Wharf/splits2/ALV_Complete_2_hl3new/envars.sh
+
+${FPControl} ${TOPOLOGY} ${FPCD} configure_flightplan --force --headerless_new
+# NOTE "start" not needed in --headerless mode
