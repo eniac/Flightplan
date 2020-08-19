6a1278e7cf10c18f09a0a844b8e83bb3ec4918ef
rakeshnagda <43049378+rakeshnagda@users.noreply.github.com>
2020-08-18 22:02:16 -0400

    Update start2.sh

--- Wharf/splits2/ALV_Complete_1_hl3new/start2.sh
+++ Wharf/splits2/ALV_Complete_1_hl3new/start2.sh
@@ -5,7 +5,7 @@ set -e
 
 echo "Starting ALV_Complete_1 (hl3new)"
 
-source /home/nsultana/2/P4Boosters/Wharf/splits2/ALV_Complete_1_hl3new/envars.sh
+source $WHARF_REPO/splits2/ALV_Complete_1_hl3new/envars.sh
 
 ${FPControl} ${TOPOLOGY} ${FPCD} configure_flightplan --force --headerless_new
 # NOTE "start" not needed in --headerless mode
