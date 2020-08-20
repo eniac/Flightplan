5a10f63b7fc5614ed1ceb543bffd363186cd44af
rakeshnagda <43049378+rakeshnagda@users.noreply.github.com>
2020-08-20 01:17:05 -0400

    Removed hardcoded paths

--- Wharf/splits2/ALV_Complete_All/start2.sh
+++ Wharf/splits2/ALV_Complete_All/start2.sh
@@ -6,10 +6,10 @@
 set -e
 
 echo "ALV_Complete_All starting ALV_Complete_2_FW"
-/home/nsultana/2/P4Boosters/Wharf/splits/ALV_Complete_2_FW/start2.sh
+$WHARF_REPO/splits/ALV_Complete_2_FW/start2.sh
 
 echo "Starting ALV_Complete_All"
-PROG_PATH=/home/nsultana/2/P4Boosters/Wharf/splits2/ALV_Complete_All
+PROG_PATH=$WHARF_REPO/splits2/ALV_Complete_All
 source ${PROG_PATH}/envars.sh
 FPCD=${PROG_PATH}/FPControlData_R2.yml
 ${FPControl} ${TOPOLOGY} ${FPCD} configure_flightplan --force
